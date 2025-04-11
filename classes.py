556e5a7cbb6ccfd9a80de6e0fece494280f17e15
Coen Konings <coen.konings@hotmail.com>
2025-04-11 14:42:11 +0200

    add fade in and stopping time

--- classes.py
+++ classes.py
@@ -3,21 +3,45 @@ TIMESTEP = 0.125
 
 class Dynamic:
     """
-    The dynamic class is used to track the dynamics of a note, from ppp to fff,
-    and gradual and sudden changes to dynamics.
+    The dynamic event class is used only to organize a set of constants for
+    readability.
 
     Dynamics from ppp to fff are stored as numbers 0 through 7. "Special"
     dynamics instructions, such as fp, are stored as negative numbers.
 
-    Changes in dynamic are stored as -1, 0 or 1, where 0 denotes unchanging
-    dynamics, -1 denotes a decrescendo and 1 denotes a crescendo.
+    Changes in dynamic are stored as -1, 0 or 1, where 0 denotes no movement,
+    -1 denotes a decrescendo and 1 denotes a crescendo.
     """
-    PPP, PP, P, MP, MF, F, FF, FFF, FP = 0, 1, 2, 3, 4, 5, 6, 7, -1
+    PPP, PP, P, MP, MF, F, FF, FFF = 0, 1, 2, 3, 4, 5, 6, 7
     CRESC, DECRESC, STATIC = 1, -1, 0
 
-    def __init__(self, dynamic, movementdir):
-        self.dynamic = dynamic
-        self.movementdir = movementdir
+    def __init__(self, value):
+        self.value = value
+        self.is_changing = False
+        self.target_dynamic = None
+        self.start_dynamic = None
+        self.time_to_reach_target = 0
+
+    def __str__(self):
+        dynamic_dict = {
+            0: "ppp",
+            1: "pp",
+            2: "p",
+            3: "mp",
+            4: "mf",
+            5: "f",
+            6: "ff",
+            7: "fff"
+        }
+        return f'[Dynamic {dynamic_dict[self.value]} {
+            "moving towards " + dynamic_dict[self.target_dynamic] if self.is_changing else "static"
+        }]'
+
+    def start_change(self, target, time):
+        self.is_changing = True
+        self.target_dynamic = target
+        self.start_dynamic = self.value
+        self.time_to_reach_target = time
 
 
 class Instrument:
@@ -37,9 +61,11 @@ class Instrument:
         self.max_note_length = max_note_length
         self.can_solo = can_solo
         self.is_playing = False
-        self.play_time = 0
-        self.stopped_playing = False
+        self.is_stopping = False
         self.instrument_group = instrument_group
+        self.play_time = None
+        self.dynamic = None
+        self.dynamic_change = None
 
     def __str__(self):
         return f'[Instrument: {self.name}]'
@@ -47,46 +73,79 @@ class Instrument:
     def start_playing(self):
         self.is_playing = True
         self.play_time = 0
-        print(self, "starts playing")
+        self.instrument_group.num_playing += 1
 
-    def stop_playing(self):
-        self.play_time = 0
-        self.is_playing = False
-        self.stopped_playing = True
-        print(self, "stops playing")
+        if not self.dynamic:
+            self.dynamic = Dynamic(Dynamic.PPP)
+
+        self.dynamic.start_change(
+            self.instrument_group.texture.dynamic.value,
+            self.instrument_group.texture.fade_time
+        )
+
+        print(
+            self,
+            "starts playing on",
+            self.dynamic,
+            end=""
+        )
+
+    def stop_playing(self, skip_stopping_process=False):
+        if not skip_stopping_process:
+            self.is_stopping = True
+            print(self, "is stopping", end="")
+        else:
+            self.is_stopping = False
+            print(self, "has stopped", end="")
+
+        self.instrument_group.num_playing -= 1
 
     def step(self):
-        self.stopped_playing = False
-        self.play_time += TIMESTEP
+        if self.play_time is not None:
+            self.play_time += TIMESTEP
 
-        if self.is_playing and self.play_time > self.max_note_length:
+        if self.is_stopping and self.play_time > self.max_note_length:
+            self.is_stopping = False
+            self.is_playing = False
+            self.play_time = 0
+            print(self, "has stopped", end="")
+
+        if self.is_playing and self.play_time > (
+            self.max_note_length - self.instrument_group.texture.fade_time
+        ) and not self.is_stopping:
             self.stop_playing()
+
 
     def can_start_playing(self):
         # If the instrument is not playing, play_time tracks the length of the
         # rest.
-        ready_to_start = self.play_time > self.instrument_group.line.rest_time
+        if self.play_time is None:
+            return True
+
+        ready_to_start = self.play_time > self.instrument_group.texture.rest_time
 
         return not self.is_playing and ready_to_start
 
+    def counts_as_playing(self):
+        return self.is_playing
 
 
 class InstrumentGroup:
     def __init__(
             self,
             groupname,
-            instrumentname,
+            instrument_name,
             pitch_range,
             max_note_length,
             size,
-            line = None,
+            texture = None,
             number_start = 1
         ):
         self.name = groupname
-        self.line = line
+        self.texture = texture
         self.instruments = [
             Instrument(
-                instrumentname + " " + str(i + number_start),
+                instrument_name + " " + str(i + number_start),
                 pitch_range,
                 self,
                 max_note_length,
@@ -104,19 +163,17 @@ class InstrumentGroup:
             print(instrument)
 
     def step(self):
-        should_start_playing = self.num_playing < self.line.max_playing
+        should_start_playing = self.num_playing < self.texture.max_playing
 
         for instrument in self.instruments:
             if should_start_playing and instrument.can_start_playing():
                 instrument.start_playing()
                 should_start_playing = False
-                self.num_playing += 1
 
             instrument.step()
-            self.num_playing -= 1 if instrument.stopped_playing else 0
 
-    def set_texture(self, line):
-        self.line = line
+    def set_texture(self, texture):
+        self.texture = texture
 
 
 class Texture:
@@ -189,11 +246,16 @@ class Piece:
     def start(self):
 
         while self.time < self.num_measures:
-            self.time += TIMESTEP
-            self.step()
-
             if self.time.is_integer():
-                print("---------")
+                print("---------", end="")
+            elif self.time % 0.25 == 0:
+                print("-", end="")
+            else:
+                print(".", end="")
+
+            self.step()
+            print("")
+            self.time += TIMESTEP
 
     def step(self):
         for line in self.lines:
--- main.py
+++ main.py
@@ -6,7 +6,7 @@ if __name__ == "__main__":
     NUM_MEASURES = int(sys.argv[1])
 
     trumpets = InstrumentGroup("trumpets", "trumpet", (58, 77), 2, 5)
-    line = Line(76, Dynamic.P, [trumpets])
+    line = Line(76, Dynamic(Dynamic.P), [trumpets])
 
     piece = Piece(70, (4, 4), NUM_MEASURES, [], [line])
     piece.start()
